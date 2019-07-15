3f5caa64dc58473a3b0921b327f7dddfc587aa96
Stefan Wurck <vitatech@gmx.de>
2019-07-15 15:14:46 +0200

    copied Starting files from previous excercise

--- README.md
+++ README.md
@@ -45,3 +45,38 @@ Try to bring your learning back to our colorConvert api and deploy it with now.
   "routes": [{ "src": "/(.*)", "dest": "/app.js" }]
 }
 ```
+
+The files I started out with were all tekaen from the previous excersize about creating a middleware
+This was my original README:
+# Implementing-Middlewares
+creating a logger middleware
+
+## Implementing Middlewares
+
+1. Create a logger middleware that logs on every request the following values
+    - Request Method (HTTP verb e.g. GET)
+    - the route that is requested
+
+## Steps I took
+creating repo on github
+cloning repo to local machine
+npm int to create a package.json
+npm install express --save to install express as a dev-dependency of my project
+create .gitignore which avoids node_modules being uploaded to github by entering
+" echo "node_modules">.gitignore" in terminal
+install nodemon as a development dependency:
+
+npm install --save-dev nodemon
+
+nodemon will start node again after every change made to the file watched and thus everything will be restarted and the localhost-browser-window will update
+from here on changes will automatically be visible in the localhost window
+
+created a script in my package.json to start  my server using nodemon, watching my file (in my case, named "index.js")
+
+"dev": "nodemon index.js"
+
+task 1.2 :
+Install "color-convert"
+(npm install color-convert)
+
+
---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const convert = require("color-convert");
+const fs = require("fs");
+const port = 3000;
+const router = express.Router();
+
+// // jQuery -> $
+// (function ($, undefined) {
+//   $('.button')
+// })(document.querySelectorAll)
+//   // Mootools -> $
+//   (function ($) {
+//     $.mootools()
+//   })(mootools)
+
+
+//How my search-parameters get converted
+const colorHandler = (req, res, colorCase) => {
+  const myColor = req.query.color.split(",").map(channel => parseInt(channel));
+  // const color = convert.rgb.colorCase(...myColor);
+  const color = convert.rgb[colorCase](...myColor);
+  const [hue, saturation, luminance] = color;
+  console.log(`Cool cat, so you found out, what ${myColor} is looking like, when being converted to ${colorCase}`);
+  return res.json({ hue, saturation, luminance });
+};
+app.use(function (req, res, next) {
+  if (!req.query.color) {
+    res.status(500).send("No color");
+    console.log("Color parameter is missing")
+  } else {
+    console.log('color there');
+    next();
+  }
+});
+app.get("/convert/:colorConversionMode", function (req, res) {
+  const { colorConversionMode } = req.params
+  switch (colorConversionMode) {
+    // in case I want to convert rgb to --- hsl
+    case "hsl":
+      colorHandler(req, res, 'hsl');
+      break;
+
+    default:
+      res.status(400).json({ error: `Route ${colorConversionMode} is not supported.` })
+      break;
+  }
+})
+
+
+
+
+
+
+
+//////////////////////////
+
+//////////////////////////////////////
+
+// router.use('/1', function (req, res, next) {
+//   console.log('Requested Route:', req.originalUrl, 'HTTP-Method used:', req.method);
+//   next();
+// }, function (req, res, next) {
+//   res.send("1. task accomplished, as far as I can tell..");
+
+// });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
+// mount the router on the app
+// app.use('/', router);
